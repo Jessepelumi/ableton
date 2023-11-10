8590f4d4d8aac8dc707a9e715a4487f19a746483
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-11-10 15:07:50 +0100

    added futura font

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className="first-image3">
+                {/* <div className="first-image3">
                     <div>
                         <img src="https://ableton-production.imgix.net/uploads/homepage-teasers/428_Note_Homepage_Tile_black_shadow.jpg" alt="" />
                         <div>
@@ -44,7 +44,44 @@ const Home = () => {
                             </Link>
                         </div>
                     </div>
+                </div> */}
+            </div>
+
+            <div className="second">
+                <div className="latest">
+                    <nav>
+                        <h2>The latest form Ableton</h2>
+
+                        <div>
+                            <Link>All posts</Link>
+                            <Link>Artists</Link>
+                            <Link>News</Link>
+                            <Link>Downloads</Link>
+                            <Link>Tutorials</Link>
+                            <Link>Videos</Link>
+                            <Link>Loops</Link>
+                        </div>
+                    </nav>
+
+                    <div>
+                        <div>
+                            <img src="https://cdn-resources.ableton.com/resources/uploads/zinnia/thumb_SHS_20_Closing_Marathon_Season_1_RESIZE.jpg.600x340_q85_crop_upscale.jpg" alt="" />
+                            <span>Artist</span>
+                            <p>Together, Apart: Remote Jamming with Speedy J</p>
+                        </div>
+                        <div>
+                            <img src="https://cdn-resources.ableton.com/resources/uploads/zinnia/thumb_P3-events-web-800x400_1.jpg.600x340_q85_crop_upscale.jpg" alt="" />
+                            <span>Artist</span>
+                            <p>Get Hands-on with Push 3 at an Event Near You</p>
+                        </div>
+                        <div>
+                            <img src="https://cdn-resources.ableton.com/resources/uploads/zinnia/thumb_432_Loop_Session_Machina.jpg.600x340_q85_crop_upscale.jpg" alt="" />
+                            <span>Artist</span>
+                            <p>Loop Session: Discover machìna's Approach to Performing</p>
+                        </div>
+                    </div>
                 </div>
+
             </div>
 
         </div>
