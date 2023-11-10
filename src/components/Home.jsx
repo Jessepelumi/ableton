84b7dadd93d31249ab2db1ef22c2cef5eda05d72
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-11-10 12:01:43 +0100

    done with home component head section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import "../sass/home.css";
 import { Link } from "react-router-dom";
+import { CaretRight } from "@phosphor-icons/react";
 
 const Home = () => {
     return (
@@ -7,17 +8,41 @@ const Home = () => {
             
             <div className="first">
                 <div className="first-image1">
-                    <img src="https://ableton-production.imgix.net/uploads/homepage-teasers/Homepage_Tile_1000x680_Mxr9Rpo.jpg" alt="" />
                     <div>
-                        <h2>Update Live and Push for free</h2>
-                        <Link>Download Live 11.3.10</Link>
+                        <img src="https://ableton-production.imgix.net/uploads/homepage-teasers/Homepage_Tile_1000x680_Mxr9Rpo.jpg" alt="" />
+                        <div>
+                            <h2>Update Live and Push for free</h2>
+                            <Link>
+                                Download Live 11.3.10
+                                <CaretRight size={12} />
+                            </Link>
+                        </div>
                     </div>
                 </div>
+
                 <div className="first-image2">
-                    <img src="https://ableton-production.imgix.net/uploads/homepage-teasers/Push-opt2_0CUDOtu.jpg?fit=crop&amp;auto=format&amp;fm=jpg" alt="" />
                     <div>
-                        <h2>Focus and feel</h2>
-                        <Link>Meet the new Push</Link>
+                        <img src="https://ableton-production.imgix.net/uploads/homepage-teasers/Push-opt2_0CUDOtu.jpg?fit=crop&amp;auto=format&amp;fm=jpg" alt="" />
+                        <div>
+                            <h2>Focus and feel</h2>
+                            <Link>
+                                Meet the new Push
+                                <CaretRight size={12} />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="first-image3">
+                    <div>
+                        <img src="https://ableton-production.imgix.net/uploads/homepage-teasers/428_Note_Homepage_Tile_black_shadow.jpg" alt="" />
+                        <div>
+                            <h2>See what's new in Note</h2>
+                            <Link>
+                                Check out the latest features
+                                <CaretRight size={12} />
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
