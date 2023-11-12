7483b2dcf08407328cbccaf9d998974e3a5d5648
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-11-13 00:09:26 +0100

    latest commit

--- src/components/Live.jsx
+++ src/components/Live.jsx
@@ -1,6 +1,6 @@
 import "../sass/live.css";
 import { Link } from "react-router-dom";
-import { PlayCircle, CaretRight } from "@phosphor-icons/react";
+import { PlayCircle, CaretRight, Plus } from "@phosphor-icons/react";
 
 const Live = () => {
     return (
@@ -63,13 +63,13 @@ const Live = () => {
                 </div>
             </div>
 
-            <div className="perfect-take">
+            <div className="live-options">
                 <div>
-                    <div className="perfect-take-heading">
+                    <div className="live-options-heading">
                         <h2>Create your perfect take</h2>
                     </div>
 
-                    <div className="perfect-take-video">
+                    <div className="live-options-video">
                         <a href="https://www.youtube.com/watch?v=StDHuDdDANc" target="_blank">
                             <figure>
                                 <img src="https://ableton-production.imgix.net/live/poster-comping.jpg?auto=compress%2Cformat&w=1680" alt="" />
@@ -80,22 +80,154 @@ const Live = () => {
                         </a>
                     </div>
 
-                    <div className="perfect-take-text">
-                        <div>
-                            <h2>Take recording and comping</h2>
-                            <p>
-                                Live organizes multiple passes of an audio or MIDI performance into individual takes. Pick the best moments of each performance and combine them to create your perfect take. Or approach sound design in a new way by splicing together random samples from your library.
-                            </p>
-                        </div>
-                        <div>
-                            <h2>Linked-Track Editing</h2>
-                            <p>
-                                Link two or more tracks to edit their content simultaneously. This makes editing multi-tracked instruments or performances with multiple musicians easy and fast whilst keeping everything in time across tracks. This feature also works for MIDI tracks.
-                            </p>
+                    <div className="live-options-text">
+                        <div className="live-options-features">
+                            <div >
+                                <h2>Take recording and comping</h2>
+                                <p>
+                                    Live organizes multiple passes of an audio or MIDI performance into individual takes. Pick the best moments of each performance and combine them to create your perfect take. Or approach sound design in a new way by splicing together random samples from your library.
+                                </p>
+                            </div>
+                            <div>
+                                <h2>Linked-Track Editing</h2>
+                                <p>
+                                    Link two or more tracks to edit their content simultaneously. This makes editing multi-tracked instruments or performances with multiple musicians easy and fast whilst keeping everything in time across tracks. This feature also works for MIDI tracks.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div className="live-options">
+                <div>
+                    <div className="live-options-heading">
+                        <h2>Add more feeling to your music</h2>
+                    </div>
+
+                    <div className="live-options-video">
+                        <a href="https://www.youtube.com/watch?v=OP6EKAggYyE" target="_blank">
+                            <figure>
+                                <img src="https://ableton-production.imgix.net/live/poster-mpe.jpg?auto=compress%2Cformat&w=1680" alt="" />
+                                <figcaption>
+                                Artist demonstrates MPE using a Roli Seaboard, Push and Live 11's new Note Expression View
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </div>
+
+                    <div className="live-options-text">
+                        <div className="live-options-features">
+                            <div >
+                                <h2>Use your MPE-capable controller</h2>
+                                <p>
+                                    Plug in your MPE-capable controller and immediately add bends, slides and pressure for each individual note in a chord. Add subtle expression variations, morph between chords and create evolving sonic textures.
+                                </p>
+                            </div>
+                            <div>
+                                <h2>What is MPE?</h2>
+                                <p>
+                                    MPE stands for MIDI Polyphonic Expression. This way of using MIDI allows MPE-capable devices to control multiple parameters of every note in real time for more expressive instrumental performances.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="learn-more">
+                <button>
+                    <Plus size={25} weight="bold" />
+                    Learn more
+                </button>
+            </div>
+
+            <div className="learn-more-text">
+                <div>
+                    <img src="https://ableton-production.imgix.net/live/screenshot-note-expression-view.jpg?auto=compress%2Cformat&w=1024" alt="" />
+                    <div>
+                        <h2>Note Expression View</h2>
+                        <p>
+                            Edit the pitch, slide and pressure envelopes of each note to refine the expression of your takes. Or take your sound design further by sequencing polyphonic sound variations.
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <img src="https://ableton-production.imgix.net/live/screenshot-expressive-sounds.jpg?auto=compress%2Cformat&w=1024" alt="" />
+                    <div>
+                        <h2>More expressive sounds included</h2>
+                        <p>
+                            Wavetable, Sampler and Arpeggiator are all updated to support MPE. And Live comes with MPE presets for each device that bring new dimensions of interaction and playability to your sound. The new expressive possibilities also enable polyphonic aftertouch on Push.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="experiment">
+
+                <div className="experiment-heading">
+                    <h2>Experiment with new devices</h2>
+                </div>
+
+            </div>
+
+            <div className="stage">
+
+                <div className="stage-heading">
+                    <h2>Do more on stage</h2>
+                </div>
+
+                <div className="stage-tempo">
+                    <div className="tempo-video">
+                        <a href="https://www.youtube.com/watch?v=OP6EKAggYyE" target="_blank">
+                            <figure>
+                                <img src="https://ableton-production.imgix.net/live/poster-mpe.jpg?auto=compress%2Cformat&w=1680" alt="" />
+                                <figcaption>
+                                Artist demonstrates MPE using a Roli Seaboard, Push and Live 11's new Note Expression View
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </div>
+                    <div className="tempo-text">
+                        <h2>Tempo following</h2>
+                        <p>
+                            Live listens to and adjusts its tempo based on incoming audio in real time, making it a dynamic part of the band instead of the tempo source that everyone has to follow. When you DJ, you can even turn Live into a tempo-synced FX box.
+                        </p>
+                    </div>
+                    <div></div>
+                </div>
+
+            </div>
+
+            <div className="stage">
+
+                <div className="stage-heading">
+                    <h2>Do more on stage</h2>
+                </div>
+
+                <div className="stage-tempo">
+                    <div className="tempo-video">
+                        <a href="https://www.youtube.com/watch?v=OP6EKAggYyE" target="_blank">
+                            <figure>
+                                <img src="https://ableton-production.imgix.net/live/poster-mpe.jpg?auto=compress%2Cformat&w=1680" alt="" />
+                                <figcaption>
+                                Artist demonstrates MPE using a Roli Seaboard, Push and Live 11's new Note Expression View
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </div>
+                    <div className="tempo-text">
+                        <h2>Tempo following</h2>
+                        <p>
+                            Live listens to and adjusts its tempo based on incoming audio in real time, making it a dynamic part of the band instead of the tempo source that everyone has to follow. When you DJ, you can even turn Live into a tempo-synced FX box.
+                        </p>
+                    </div>
+                    <div></div>
+                </div>
+
+            </div>
+
+
 
         </div>
     );
