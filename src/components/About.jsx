627b21e50eb92cc349412faf5a36a48b896e87d0
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-11-13 23:46:52 +0100

    nearly done with about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../sass/about.css";
-import { PlayCircle } from "@phosphor-icons/react";
+import { PlayCircle, CaretRight } from "@phosphor-icons/react";
 
 const About = () => {
 
@@ -31,10 +31,10 @@ const About = () => {
                     </p>
                 </div>
                 <div className="header-image2">
-                    <div className="first">
+                    <div className="image2-first">
                         <img src="https://ableton-production.imgix.net/about/photo-1.jpg" alt="" />
                     </div>
-                    <div className="second">
+                    <div className="image2-second">
                         <img src="https://ableton-production.imgix.net/about/photo-2.jpg" alt="" />
                     </div>
                 </div>
@@ -67,6 +67,19 @@ const About = () => {
                         Most of us are active musicians, producers, and DJs, and many of us use Live and Push every day. We come from a wide range of cultural and professional backgrounds. Some of us have PhDs, some are self-taught, and most of us are somewhere in between. What connects us is the shared belief that each of us has the skills and knowledge to contribute to something big: helping to shape the future of music culture.
                     </p>
                 </div>
+                <div className="header-image3">
+                    <div className="image3-first">
+                        <div>
+                            <img src="https://ableton-production.imgix.net/about/photo-3.jpg" alt="" />
+                        </div>
+                        <div>
+                            <img src="https://ableton-production.imgix.net/about/photo-4.jpg" alt="" />
+                        </div>
+                    </div>
+                    <div className="image3-second">
+                        <img src="https://ableton-production.imgix.net/about/photo-5.jpg" alt="" />
+                    </div>
+                </div>
                 <div className="header-text">
                     <h2>
                         We believe it takes focus to create truly outstanding instruments. We only work on a few products and we strive to make them great.
@@ -74,6 +87,9 @@ const About = () => {
                     <p>
                         Rather than having a one-size-fits-all process, we try to give our people what they need to work their magic and grow. We've learned that achieving the best results comes from building teams that are richly diverse, and thus able to explore problems from a wider set of perspectives. We don't always agree with each other, but opinion and debate are valued and openly encouraged.
                     </p>
+                </div>
+                <div className="header-image4">
+                    <img src="https://ableton-production.imgix.net/about/poster-meet-the-makers.jpg?auto=format&dpr=2&fit=crop&fm=jpg&ixjsv=1.1.3&q=50&w=1200" alt="" />
                 </div>
                 <div className="header-text">
                     <h2>
@@ -84,6 +100,16 @@ const About = () => {
                         Alongside an internal training program, employees are actively supported in acquiring new knowledge and skills, and coached on applying these in their daily work. In addition, staff-organized development and music salons are a chance to discuss new technologies, production techniques and best practices.
                     </p>
                 </div>
+                <div className="header-image5">
+                    <div className="image5-container">
+                        <div className="image5-first">
+                            <img src="https://ableton-production.imgix.net/about/photo-6-a.jpg" alt="" />
+                        </div>
+                        <div className="image5-second">
+                            <img src="https://ableton-production.imgix.net/about/photo-7.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
                 <div className="header-text">
                     <h2>
                         We want our employees to love it here. Since we’re looking for exceptional talent from around the world, we will do everything we can to make your transition as easy as possible.
@@ -91,6 +117,20 @@ const About = () => {
                     <p>
                         If you're joining us in Berlin, we'll help with relocation and paperwork. We'll even provide you with free German or English lessons. Plus, working in Germany means you can expect comprehensive health insurance for you and your family, as well as generous maternity and paternity leave. Office hours are flexible, but it's not all work; we have several company and team outings throughout the year as well as a variety of fun, informal small-group activities.
                     </p>
+                </div>
+                <div className="header-image6">
+                    <div>
+                        <div className="image6">
+                            <img src="https://ableton-production.imgix.net/about/photo-8.jpg" alt="" />
+                        </div>
+                        <div className="image6-text">
+                            <h2>We're really proud of the work we've done so far. But there's so much more to come. If you'd like to be a part of it, please join us.</h2>
+                            <Link>
+                                See latest jobs
+                                <CaretRight size={20} />
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
 
